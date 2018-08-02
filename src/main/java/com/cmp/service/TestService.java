41688070af169be8b0cc71009a75f185b95b83f6
EnowshWong <1059678688@qq.com>
2018-08-02 14:19:02 +0800

    项目搭建

---
+++ src/main/java/com/cmp/service/TestService.java
@@ -0,0 +1,17 @@
+package com.cmp.service;
+
+import com.cmp.common.pojo.TaoTaoResult;
+import com.cmp.pojo.TbUser;
+
+import java.util.List;
+
+/**
+ * @author:wong
+ */
+public interface TestService {
+    //定义服务接口
+
+    List<TbUser> getUserList();
+    TaoTaoResult createUser(TbUser tbUser);
+    TbUser getUserByUsername(String username);
+}
