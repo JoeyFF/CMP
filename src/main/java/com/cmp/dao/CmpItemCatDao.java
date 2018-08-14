f4b64dd162fa390442eaa2e807a0b511eef9929d
new-zdb <zy13925zdb@gmail.com>
2018-08-14 11:54:34 +0800

    对dao层增加@Repository注解
    对dao层增加@Repository注解，否则在
    @Autowired的时候会报错

--- src/main/java/com/cmp/dao/CmpItemCatDao.java
+++ src/main/java/com/cmp/dao/CmpItemCatDao.java
@@ -4,7 +4,9 @@ import com.cmp.pojo.CmpItemCat;
 import com.cmp.pojo.CmpItemCatQuery;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CmpItemCatDao {
     int countByExample(CmpItemCatQuery example);
 
